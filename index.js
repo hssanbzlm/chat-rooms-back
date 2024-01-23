60f19219e2c79eddaccd5531ab22971b36b352ce
Hassan Bouzlima <bouzlima.h@live.fr>
2024-01-23 18:37:31 +0100

    Add error handling and protect as middlewares

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const userRouter = require("./routes/user.router");
 const chatRoomRouter = require("./routes/chat-room.router");
 const envConfig = require("./config");
 const connectDb = require("./database/mongo.database");
+const { protect } = require("./middlewares/protect.middleware");
+const { errorHandler } = require("./middlewares/error.middleware");
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/chat-room", chatRoomRouter);
-
+app.use(protect);
+app.use(errorHandler);
 const start = async () => {
   await connectDb();
   app.listen(envConfig.port, () => {
