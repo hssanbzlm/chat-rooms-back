373a3815585df2061944b51c1525a160923242db
Hassan Bouzlima <bouzlima.h@live.fr>
2024-01-23 18:34:04 +0100

    Fix protect middleware

--- middlewares/protect.middleware.js
+++ middlewares/protect.middleware.js
@@ -1,7 +1,8 @@
 const { verifyToken } = require("../utils/token.util");
 const { key } = require("../config");
 module.exports.protect = (req, res, next) => {
+  const token = req.cookies["auth-token"];
   const verif = verifyToken(token, key);
   if (verif) next();
-  else res.status(403).send("access forbidden");
+  else res.end("You are not authorized");
 };
