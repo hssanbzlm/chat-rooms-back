8ebde7012a3f3db996621c9ee4feb25de3ce0115
Hassan Bouzlima <bouzlima.h@live.fr>
2024-01-24 11:04:20 +0100

    Update auth and protect

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,21 +4,23 @@ const { signToken } = require("../utils/token.util");
 const { key } = require("../config");
 
 module.exports.join = async (req, res) => {
-  const { userName, roomCode } = req.body;
-  const userDoc = await user.findOne({ userName });
-  const roomDoc = await chatRoom.findOne({ roomCode });
-  const options = {
-    httpOnly: true,
-    secure: true,
-  };
-  if (userDoc && roomDoc) {
-    const token = signToken({ userName, roomCode }, key);
-    res.cookie("auth-token", token, options);
-    res.status(200).send("you joined a room successfully");
-  } else res.status(404).send("error while joining a room");
+  if (!req.cookies["auth-token"]) {
+    const { userName, roomCode } = req.body;
+    const userDoc = await user.findOne({ userName });
+    const roomDoc = await chatRoom.findOne({ roomCode });
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+    if (userDoc && roomDoc) {
+      const token = signToken({ userName, roomCode }, key);
+      res.cookie("auth-token", token, options);
+      res.status(200).send("you joined a room successfully");
+    } else res.status(404).send("error while joining a room");
+  } else res.status(200).send("You are already inside a room");
 };
 
-module.exports.leave = async (req, res) => {
+module.exports.leave = (req, res) => {
   res.clearCookie("auth-token");
-  res.send("ok");
+  res.status(200).send("ok");
 };
--- middlewares/protect.middleware.js
+++ middlewares/protect.middleware.js
@@ -2,7 +2,11 @@ const { verifyToken } = require("../utils/token.util");
 const { key } = require("../config");
 module.exports.protect = (req, res, next) => {
   const token = req.cookies["auth-token"];
-  const verif = verifyToken(token, key);
-  if (verif) next();
-  else res.end("You are not authorized");
+  if (token) {
+    const userInfo = verifyToken(token, key);
+    if (userInfo) {
+      req.userInfo = userInfo;
+      next();
+    } else res.end("You are not authorized");
+  } else res.end("You are not authorized");
 };
