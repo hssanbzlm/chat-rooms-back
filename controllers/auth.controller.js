b87552a4d46d88a6c0ca7b480d18b8c59d5f27c4
Hassan Bouzlima <bouzlima.h@live.fr>
2024-01-25 19:21:08 +0100

    Use token name as env variable

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,10 @@
 const user = require("../models/user.model");
 const chatRoom = require("../models/chat-room.model");
 const { signToken } = require("../utils/token.util");
-const { key } = require("../config");
+const { key, authTokenName } = require("../config");
 
 module.exports.join = async (req, res) => {
-  if (!req.cookies["auth-token"]) {
+  if (!req.cookies[authTokenName]) {
     const { userName, roomCode } = req.body;
     const userDoc = await user.findOne({ userName });
     const roomDoc = await chatRoom.findOne({ roomCode });
@@ -14,13 +14,13 @@ module.exports.join = async (req, res) => {
     };
     if (userDoc && roomDoc) {
       const token = signToken({ userName, roomCode }, key);
-      res.cookie("auth-token", token, options);
+      res.cookie(authTokenName, token, options);
       res.status(200).send("you joined a room successfully");
     } else res.status(404).send("error while joining a room");
   } else res.status(200).send("You are already inside a room");
 };
 
 module.exports.leave = (req, res) => {
-  res.clearCookie("auth-token");
+  res.clearCookie(authTokenName);
   res.status(200).send("ok");
 };
--- middlewares/protect.middleware.js
+++ middlewares/protect.middleware.js
@@ -1,7 +1,8 @@
 const { verifyToken } = require("../utils/token.util");
 const { key } = require("../config");
+const { authTokenName } = require("../config");
 module.exports.protect = (req, res, next) => {
-  const token = req.cookies["auth-token"];
+  const token = req.cookies[authTokenName];
   if (token) {
     const userInfo = verifyToken(token, key);
     if (userInfo) {
