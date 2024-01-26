8ddbd5bb15acb3c1de050977e0e7c53371a9a8b5
Hassan Bouzlima <bouzlima.h@live.fr>
2024-01-26 16:08:45 +0100

    Fix auth router typo

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { join, leave } = require("../controllers/auth.controller");
 const { asyncErrorHandler } = require("../middlewares/error.middleware");
-router.get("/join", asyncErrorHandler(join));
+router.post("/join", asyncErrorHandler(join));
 router.post("/leave", leave);
 
 module.exports = router;
