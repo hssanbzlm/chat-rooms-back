d4e4e2cf66af60686a40bef8ff27b23d336da598
Hassan Bouzlima <bouzlima.h@live.fr>
2024-01-26 14:09:46 +0100

    Add user-join and user-leave emits

--- listeners/socket.js
+++ listeners/socket.js
@@ -1,6 +1,7 @@
 const { Server } = require("socket.io");
 const { verifyToken } = require("../utils/token.util");
 const { key, authTokenName } = require("../config");
+const { saveMessage } = require("../controllers/message.controller");
 module.exports = (server) => {
   const io = new Server(server);
   io.use((socket, next) => {
@@ -14,16 +15,31 @@ module.exports = (server) => {
       next(new Error("Invalid"));
     }
   });
-  io.on("connection", async (socket) => {
-    console.log(socket.id, "connected");
+  io.on("connection", (socket) => {
     const cookie = socket.handshake.headers.cookie;
     const authToken = cookie.split(`${authTokenName}=`)[1];
     const { roomCode, userName } = verifyToken(authToken, key);
     socket.join(roomCode);
-    socket.on("send-message", (data) => {
+    socket
+      .to(roomCode)
+      .emit("user-join", { data: `User ${userName} joined the chat` });
+    socket.on("send-message", async (data) => {
+      const saved = await saveMessage({
+        userName,
+        roomCode,
+        content: data,
+      }).catch((err) => console.log(err));
+      if (saved) {
+        socket
+          .to(roomCode)
+          .emit("new-message", { message: data, user: userName });
+      }
+    });
+
+    socket.on("disconnect", (reason) => {
       socket
         .to(roomCode)
-        .emit("new-message", { message: data, user: userName });
+        .emit("user-leave", { data: `User ${userName} left the chat` });
     });
   });
 };
