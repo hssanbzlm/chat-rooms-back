1f6abc023da92b5f1448c19151f436b18eedf1ac
Hassan Bouzlima <bouzlima.h@live.fr>
2024-01-29 15:56:12 +0100

    Add is-auth router

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
-const { join, leave } = require("../controllers/auth.controller");
+const { join, leave, getUserInfo } = require("../controllers/auth.controller");
 const { asyncErrorHandler } = require("../middlewares/error.middleware");
+const { protect } = require("../middlewares/protect.middleware");
+router.get("/is-auth", protect, getUserInfo);
 router.post("/join", asyncErrorHandler(join));
 router.post("/leave", leave);
 
