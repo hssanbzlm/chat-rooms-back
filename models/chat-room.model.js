a97093371fcaef357cfc9ec6ec96ce6e43d19c3c
Hassan Bouzlima <bouzlima.h@live.fr>
2024-01-30 18:56:48 +0100

    Use findOneAndDelete instead of deleteOne and add post delete hook

--- models/chat-room.model.js
+++ models/chat-room.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const messageShema = require("./message.model");
 
 const chatRoomSchema = new mongoose.Schema({
   roomName: String,
@@ -7,6 +8,13 @@ const chatRoomSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "user",
   },
+});
+
+chatRoomSchema.post("findOneAndDelete", async (doc) => {
+  if (doc._id) {
+    const roomId = doc._id;
+    await messageShema.deleteMany({ receiver: roomId });
+  }
 });
 
 module.exports = mongoose.model("chat-room", chatRoomSchema);
