86924397c772683ed5154517e3d17b5e11cbd9b3
Hassan Bouzlima <bouzlima.h@live.fr>
2024-02-05 09:52:59 +0100

    #2 Add missing protect middleware

--- routes/chat-room.router.js
+++ routes/chat-room.router.js
@@ -4,7 +4,8 @@ const {
   DeleteRoom,
 } = require("../controllers/chat-room.controller");
 const { isAdmin } = require("../middlewares/user.middleware");
+const { protect } = require("../middlewares/protect.middleware");
 router.post("/create", createRoom);
-router.delete("/destroy", isAdmin, DeleteRoom);
+router.delete("/destroy", protect, isAdmin, DeleteRoom);
 
 module.exports = router;
