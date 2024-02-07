53f1eff373f7a2bc352b55c6c58c3c778908b642
Hassan Bouzlima <bouzlima.h@live.fr>
2024-02-07 15:31:46 +0100

    #21 handle multiple sockets for the same user

--- listeners/socket.room-utils.js
+++ listeners/socket.room-utils.js
@@ -1,5 +1,10 @@
 module.exports.getConnectedUsersByRoom = async (io, roomCode) => {
   const sockets = await io.in(roomCode).fetchSockets();
-  const connectedUsers = sockets.map((socket) => socket.data);
-  return connectedUsers;
+  const uniqueSockets = sockets
+    .filter(
+      (socket, index) =>
+        sockets.findIndex(({ data }) => data._id == socket.data._id) === index
+    )
+    .map((sockets) => sockets.data);
+  return uniqueSockets;
 };
