e3250b4520b3ef3f3eac498faa7179fc1768caa1
Hssan Bouzlima <bouzlima.h@live.fr>
2024-02-13 16:09:37 +0100

    Feature/update user (#31)
    * #5: Update user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,6 @@
 const user = require("../models/user.model");
-
+const { signToken } = require("../utils/token.util");
+const { key, authTokenName } = require("../config");
 module.exports.addUser = async (req, res) => {
   const { userName, fullName } = req.body;
   const doc = await user.create({ userName, fullName });
@@ -13,10 +14,29 @@ module.exports.addUser = async (req, res) => {
 module.exports.updateUser = async (req, res) => {
   const { userName } = req.userInfo;
   const { fullName } = req.body;
-  const userDoc = await user.findOneAndUpdate(
-    { userName },
-    { avatar: req.avatar, fullName }
-  );
-  if (userDoc) res.status(200).send("User updated successfully");
-  else res.status(400).send("Error while updating the user");
+  const { avatar } = req;
+  const updateFields = {
+    ...(avatar && { avatar }),
+    ...(fullName && { fullName }),
+  };
+  const options = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+  };
+  const userDoc = await user.findOneAndUpdate({ userName }, updateFields, {
+    new: true,
+  });
+  if (userDoc) {
+    const token = signToken(
+      {
+        ...req.userInfo,
+        ...updateFields,
+      },
+      key
+    );
+    res.clearCookie(authTokenName);
+    res.cookie(authTokenName, token, options);
+    res.status(200).json(userDoc);
+  } else res.status(400).send("Error while updating the user");
 };
