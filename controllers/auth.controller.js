321b5554bc3504fa06a5630546ee38aa573d394b
Hassan Bouzlima <bouzlima.h@live.fr>
2024-02-15 11:21:24 +0100

    Add roomName info to cookies

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,8 +5,8 @@ const { key, authTokenName } = require("../config");
 const { options } = require("../utils/cookie.util");
 
 module.exports.getUserInfo = (req, res) => {
-  const { userName, fullName, isAdmin, avatar } = req.userInfo;
-  res.status(200).json({ userName, fullName, isAdmin, avatar });
+  const { userName, fullName, isAdmin, avatar, roomName } = req.userInfo;
+  res.status(200).json({ userName, fullName, roomName, isAdmin, avatar });
 };
 
 module.exports.join = async (req, res) => {
@@ -22,6 +22,7 @@ module.exports.join = async (req, res) => {
           userName,
           fullName: userDoc.fullName,
           roomCode,
+          roomName: roomDoc.roomName,
           isAdmin,
           avatar: userDoc.avatar,
         },
@@ -31,6 +32,7 @@ module.exports.join = async (req, res) => {
       res.status(200).json({
         userName,
         fullName: userDoc.fullName,
+        roomName: roomDoc.roomName,
         isAdmin,
         avatar: userDoc.avatar,
       });
