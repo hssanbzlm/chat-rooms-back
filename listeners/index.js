d3110b4f30ba08c53a1147dc03022ffbf6a3dde1
Hassan Bouzlima <bouzlima.h@live.fr>
2024-02-19 12:04:05 +0100

    Add private chat listeners

--- listeners/index.js
+++ listeners/index.js
@@ -2,6 +2,7 @@ const { Server } = require("socket.io");
 const envConfig = require("../config");
 const socketMiddleware = require("./socket.middleware");
 const registerRoomHandler = require("./socket.room-handler");
+const registerPrivateHandler = require("./socket.private-handler");
 
 module.exports = (server) => {
   const io = new Server(server, {
@@ -16,6 +17,7 @@ module.exports = (server) => {
   io.on("connection", async (socket) => {
     try {
       await registerRoomHandler(io, socket);
+      await registerPrivateHandler(io, socket);
     } catch (err) {
       console.log(err);
     }
