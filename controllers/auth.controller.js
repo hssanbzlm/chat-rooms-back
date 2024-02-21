efe6767727a9a5e6dd5e13d1e0b7ade92602801c
Hassan Bouzlima <bouzlima.h@live.fr>
2024-02-21 10:52:12 +0100

    Rename roomName to belongsTo

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,11 +5,11 @@ const { key, authTokenName } = require("../config");
 const { options } = require("../utils/cookie.util");
 
 module.exports.getUserInfo = (req, res) => {
-  const { userName, fullName, isAdmin, avatar, roomName, userId } =
+  const { userName, fullName, isAdmin, avatar, belongsTo, userId } =
     req.userInfo;
   res
     .status(200)
-    .json({ userName, userId, fullName, roomName, isAdmin, avatar });
+    .json({ userName, userId, fullName, belongsTo, isAdmin, avatar });
 };
 
 module.exports.join = async (req, res) => {
@@ -26,7 +26,7 @@ module.exports.join = async (req, res) => {
           fullName: userDoc.fullName,
           roomCode,
           roomId: roomDoc._id,
-          roomName: roomDoc.roomName,
+          belongsTo: roomDoc.roomName,
           isAdmin,
           avatar: userDoc.avatar,
         },
@@ -37,7 +37,7 @@ module.exports.join = async (req, res) => {
         userId: userDoc._id,
         userName,
         fullName: userDoc.fullName,
-        roomName: roomDoc.roomName,
+        belongsTo: roomDoc.roomName,
         isAdmin,
         avatar: userDoc.avatar,
       });
