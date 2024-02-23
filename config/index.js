413f13cb6aa00ab27ac5d64697bdb509793523c7
Hassan Bouzlima <bouzlima.h@live.fr>
2024-02-23 09:15:02 +0100

    Update env variable name to match env specified by render

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-const env = process.env.NODE_ENV || "development";
+const env = process.env.MIX_ENV || "development";
 envConfig = {};
 if (env === "development") {
   envConfig = require("./development");
