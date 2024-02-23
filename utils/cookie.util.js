ddc1414b201356f6c6e65477afaa85a446f804d6
Hassan Bouzlima <bouzlima.h@live.fr>
2024-02-23 12:52:32 +0100

    Use sameSite none

--- utils/cookie.util.js
+++ utils/cookie.util.js
@@ -2,6 +2,5 @@ const envConfig = require("../config");
 module.exports.options = {
   httpOnly: true,
   secure: true,
-  sameSite: "strict",
-  domain: "chat-rooms-hssanbzlm.onrender.com",
+  sameSite: "none",
 };
