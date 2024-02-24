206e79150fc8790c3f64064185ccd3fc70a5241b
Hassan Bouzlima <bouzlima.h@live.fr>
2024-02-24 14:49:04 +0100

    Add try/catch block to getConnectedUsersByRoom function

--- listeners/socket.room-utils.js
+++ listeners/socket.room-utils.js
@@ -1,11 +1,16 @@
+const { logger } = require("../logger/logger");
 module.exports.getConnectedUsersByRoom = async (io, roomCode) => {
-  const sockets = await io.in(roomCode).fetchSockets();
-  const uniqueSockets = sockets
-    .filter(
-      (socket, index) =>
-        sockets.findIndex(({ data }) => data.userId == socket.data.userId) ===
-        index
-    )
-    .map((sockets) => sockets.data);
-  return uniqueSockets;
+  try {
+    const sockets = await io.in(roomCode).fetchSockets();
+    const uniqueSockets = sockets
+      .filter(
+        (socket, index) =>
+          sockets.findIndex(({ data }) => data.userId == socket.data.userId) ===
+          index
+      )
+      .map((sockets) => sockets.data);
+    return uniqueSockets;
+  } catch (err) {
+    logger.error(err);
+  }
 };
