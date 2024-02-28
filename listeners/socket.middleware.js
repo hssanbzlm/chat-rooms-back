c5930d61da976101cea3fd67602247e2701d755d
Hassan Bouzlima <bouzlima.h@live.fr>
2024-02-28 11:51:55 +0100

    Fix bug related to empty auth token cookie

--- listeners/socket.middleware.js
+++ listeners/socket.middleware.js
@@ -4,14 +4,24 @@ const { verifyToken } = require("../utils/token.util");
 module.exports = (socket, next) => {
   const cookie = socket.handshake.headers.cookie;
   if (cookie) {
-    const authToken = cookie
+    const authCookie = cookie
       .split(";")
-      .find((v) => v.includes(`${authTokenName}=`))
-      .split(`${authTokenName}=`)[1];
-    const decodedToken = verifyToken(authToken, authTokenKey);
-    if (decodedToken) {
-      socket.data = decodedToken;
-      next();
+      .find((v) => v.includes(`${authTokenName}=`));
+    if (authCookie) {
+      const authToken = authCookie.split(`${authTokenName}=`)[1];
+      if (authToken) {
+        const decodedToken = verifyToken(authToken, authTokenKey);
+        if (decodedToken) {
+          socket.data = decodedToken;
+          next();
+        } else {
+          socket.disconnect();
+          next(new Error("Invalid"));
+        }
+      } else {
+        socket.disconnect();
+        next(new Error("Invalid"));
+      }
     } else {
       socket.disconnect();
       next(new Error("Invalid"));
